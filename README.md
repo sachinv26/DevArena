731dbc53bc5b619b0d4f5f2c8edb2240c521bd6f
sachinv26 <vermas7008@gmail.com>
2023-07-31 01:10:44 +0530

    Feat:Project Completed

---
+++ README.md
@@ -0,0 +1,6 @@
+# DevArena
+A Social Network for developers
+<br/>
+![home-1](https://user-images.githubusercontent.com/63954124/128591365-3ce2dc6c-5405-45b2-9f76-ac2b035899eb.png)
+<br/>
+
---
+++ config/keys_dev.js
@@ -0,0 +1,7 @@
+// These credentials were created only for development purposes
+module.exports = {
+  oldMLABMongoURI: "",
+  mongoURI:
+    "mongodb+srv://sachinv26:123vermasv@cluster0.a62wak3.mongodb.net/",
+  secretOrKey: "secret",
+};
