5e07be8824557826bdf5838f028c5f95c63c255f
sachinv26 <vermas7008@gmail.com>
2023-08-01 01:45:22 +0530

    server changed

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 10000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
